2a52aa9390df12d6bd9d5985ff71a6ac97ef4841
subtainishfaqstudent <subtain>
2016-10-29 15:40:55 +0500

    authentication

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var morgan      = require('morgan');
+var passport	= require('passport');
+//var User        = require('./app/models/user'); // get the mongoose model
 var games = require('./routes/games'); //routes are defined here
 var letsplays = require('./routes/letsplays'); //routes are defined here
 var news = require('./routes/news'); //routes are defined here
 var walkthrough = require('./routes/walkthroughs'); //routes are defined here
-
+var index = require('./routes/index'); //routes are defined here
 var app = express(); //Create the Express app
+var config      = require('./config/database');
+var authentication=require('./routes/users');
 
-//connect to our database
-//Ideally you will obtain DB details from a config file
-var dbName = 'gameDB';
-var connectionString = 'mongodb://localhost:27017/' + dbName;
 
-mongoose.connect(connectionString);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(morgan('dev'));
+app.use(passport.initialize());
 
 //this for localhost
 app.use(function (req,res,next) {
-    res.header("Access-Control-Allow-Origin","*");/*
-    res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers","Content-Type");
-*/    next();
+    res.header("Access-Control-Allow-Origin","*");
+   next();
 
 });
+
+
+app.use('/',index);
+mongoose.connect(config.database);
+require('./config/passport')(passport);
+
+// bundle our routes
+// create a new user account (POST http://localhost:8080/api/signup)
+
+app.use('/api', authentication);
 app.use('/api', games); //This is our route middleware
 app.use('/api', letsplays); //This is our route middleware
 app.use('/api', news); //This is our route middleware
@@ -34,39 +44,3 @@ app.use('/api', walkthrough); //This is our route middleware
 
 module.exports = app;
 
-/*
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
-*/
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,86 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
+var jwt    = require('jwt-simple');
+var config      = require('../config/database');
+var passport	= require('passport');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+
+
+router.post('/signup', function(req, res) {
+  if (!req.body.name || !req.body.password) {
+    res.json({success: false, msg: 'Please pass name and password.'});
+  } else {
+    var newUser = new User({
+      name: req.body.name,
+      password: req.body.password,
+      userrole:req.body.userrole
+    });
+    // save the user
+    newUser.save(function(err) {
+      if (err) {
+        return res.json({success: false, msg: 'Username already exists.'});
+      }
+      res.json({success: true, msg: 'Successful created new user.'});
+    });
+  }
 });
 
+router.post('/authenticate', function(req, res) {
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.send({success: false, msg: 'Authentication failed. User not found.'});
+    } else {
+      // check if password matches
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (isMatch && !err) {
+          console.log(user);
+          // if user is found and password is right create a token
+          var token = jwt.encode(user, config.secret);
+          // return the information including token as JSON
+          res.json({success: true, token: 'JWT ' + token});
+        } else {
+          res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+        }
+      });
+    }
+  });
+});
+router.get('/memberinfo', passport.authenticate('jwt', { session: false}), function(req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    var decoded = jwt.decode(token, config.secret);
+    User.findOne({
+      name: decoded.name
+    }, function(err, user) {
+      if (err) throw err;
+
+      if (!user) {
+        return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+      } else {
+        res.json({success: true, msg: 'Welcome in the member area ' + user.name + ' role ' + decoded.userrole});
+      }
+    });
+  } else {
+    return res.status(403).send({success: false, msg: 'No token provided.'});
+  }
+});
+
+getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
 module.exports = router;
